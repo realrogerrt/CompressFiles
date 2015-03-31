513eb5eb067ae7e533321a0c1a8b2d4840670149
Roger Rodriguez Texido <rogerkael@mypcusa.com>
2015-03-30 22:33:51 -0400

    fsdfds

--- CompressFiles/Migrations/201503310159559_fileModel.cs
+++ CompressFiles/Migrations/201503310159559_fileModel.cs
@@ -7,10 +7,30 @@ namespace CompressFiles.Migrations
     {
         public override void Up()
         {
+            CreateTable(
+                "dbo.UploadedFiles",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        FileName = c.String(nullable: false, maxLength: 15),
+                        Extension = c.String(maxLength: 3),
+                        DateTime = c.DateTime(nullable: false),
+                        OriginalSize = c.Int(nullable: false),
+                        ConvertedSize = c.Int(nullable: false),
+                        OwnerUserID = c.Int(nullable: false),
+                        OwnerUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.AspNetUsers", t => t.OwnerUser_Id)
+                .Index(t => t.OwnerUser_Id);
+            
         }
         
         public override void Down()
         {
+            DropForeignKey("dbo.UploadedFiles", "OwnerUser_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.UploadedFiles", new[] { "OwnerUser_Id" });
+            DropTable("dbo.UploadedFiles");
         }
     }
 }
