d9d42bc93f7757553adf9e7672f634863322824a
Roger Rodriguez Texido <rogerkael@mypcusa.com>
2015-04-01 21:45:02 -0400

    Remote database

--- CompressFiles/Models/UploadedFile.cs
+++ CompressFiles/Models/UploadedFile.cs
@@ -11,18 +11,24 @@ namespace CompressFiles.Models
     {
         [Key]
         public int ID { get; set; }
+
         [StringLength(50)]
         [Required]
         public string FileName { get; set; }
+        
         [StringLength(4)]
         public string Extension { get; set; }
+        
         public DateTime DateTime { get; set; }
+        
         [Required]
         public int OriginalSize { get; set; }
+        
         [Required]
         public int ConvertedSize { get; set; }
 
         public string OwnerUserID { get; set; }
+        
         public ApplicationUser OwnerUser { get; set; }
     }
 }
