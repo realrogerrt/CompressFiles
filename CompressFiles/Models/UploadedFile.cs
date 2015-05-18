118e4037b99f768e0d33d7a03e084547335b8bd4
Roger Rodriguez Texido <rogerkael@gmail.com>
2015-05-18 11:59:41 -0400

    Added model description

--- CompressFiles/Models/UploadedFile.cs
+++ CompressFiles/Models/UploadedFile.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Web;
 
 namespace CompressFiles.Models
-{
+{   //This file represents a uploaded file in the application.
     public class UploadedFile
     {
         [Key]
